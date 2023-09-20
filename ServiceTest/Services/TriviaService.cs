9b4789f3c723c03330e71b691ca8c1409cf982f3
User <User@LAB3-3>
2023-09-20 15:09:37 +0300

    UPDATED

---
+++ ServiceTest/Services/TriviaService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ServiceTest.Services
+{
+    public class TriviaService
+    {
+        private HttpClient client;
+        private JsonSerializerOptions options;
+        const string url = @"https://zr8z94hw-44376.euw.devtunnels.ms/AmericanQuestions/";
+
+        public TriviaService()
+        {
+            client = new HttpClient();
+            options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true, WriteIndented=true };
+        }
+
+        public async Task<string> CheckConnection()
+        {
+
+            try
+            {
+                var response = await client.GetAsync($"{url}GetQuestions");
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return content;
+                }
+                else
+                    return "Error";
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+            return null;
+        }
+
+
+
+    }
+}
--- ServiceTest/ViewModels/MainPageViewModel.cs
+++ ServiceTest/ViewModels/MainPageViewModel.cs
@@ -1,7 +1,10 @@
 ﻿//using ServiceTest.Models;
 //using ServiceTest.Services;
+
+using ServiceTest.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +12,25 @@ using System.Windows.Input;
 
 namespace ServiceTest.ViewModels
 {
-    public class MainPageViewModel:ViewModel
+    public class MainPageViewModel : ViewModel
     {
-       
-        public MainPageViewModel()
+        private TriviaService service;
+        private string text;
+        public ICommand GetText { get; protected set; }
+        public string Text { get { return text; } set { if (text != value) {  text= value; OnPropertyChange(); } } }
+
+        public MainPageViewModel(TriviaService t)
         {
-            
+            service = t;
+            GetText = new Command(async () => { Text = await service.CheckConnection(); });
         }
+
+       
+    }
+
 
        
 
 
     }
-}
+
